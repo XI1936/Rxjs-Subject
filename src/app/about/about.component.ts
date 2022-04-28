10627becd75fc995b85602e3199c3f0c2c9488de
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2022-04-28 12:26:30 +0530

    type of Subjects

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,26 +1,64 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge} from 'rxjs';
-import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
-import {createHttpObservable} from '../common/util';
-
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import {
+  concat,
+  fromEvent,
+  interval,
+  noop,
+  observable,
+  Observable,
+  of,
+  timer,
+  merge,
+  Subject,
+  BehaviorSubject,
+  AsyncSubject,
+  ReplaySubject,
+} from "rxjs";
+import { delayWhen, filter, map, take, timeout } from "rxjs/operators";
+import { createHttpObservable } from "../common/util";
 
 @Component({
-    selector: 'about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
-
-    ngOnInit() {
-
-
-    }
-
-
+  ngOnInit() {
+    //Plan Subject which supports early Subscription
+    // const subject = new Subject();
+    // const series1$ = subject.asObservable();
+    // subject.next(1);
+    // subject.next(2);
+    // series1$.subscribe((val) => console.log(val));
+    // subject.next("After subscribe " + 3);
+    //-------------Behaviour Suject-------------------
+    // const subject = new BehaviorSubject(0);
+    // const series1$ = subject.asObservable();
+    // series1$.subscribe((val) => console.log("Early subscription", val));
+    // subject.next(1);
+    // subject.next(2);
+    // // subject.complete();
+    // setTimeout(() => {
+    //   series1$.subscribe((val) => console.log("Late subscription", val));
+    //   subject.next(3);
+    // }, 2000);
+    //-------------Async Suject-------------------
+    // const subject = new AsyncSubject();
+    // const series1$ = subject.asObservable();
+    // series1$.subscribe((val) => console.log("Early subscription", val));
+    // subject.next(1);
+    // subject.next(2);
+    // // subject.complete();
+    //-------------ReplaySuject-------------------
+    // const subject = new ReplaySubject();
+    // const series1$ = subject.asObservable();
+    // series1$.subscribe((val) => console.log("Early subscription", val));
+    // subject.next(1);
+    // subject.next(2);
+    //  //subject.complete();
+    // setTimeout(() => {
+    //   series1$.subscribe((val) => console.log("Late subscription", val));
+    //   //subject.next(3);
+    // }, 2000);
+  }
 }
-
-
-
-
-
-
