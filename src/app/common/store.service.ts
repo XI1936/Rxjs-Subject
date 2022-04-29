56e035db502620b4f5d38a4cdf8dac0ffc674724
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2022-04-29 11:25:57 +0530

    course dialog updated

--- src/app/common/store.service.ts
+++ src/app/common/store.service.ts
@@ -3,6 +3,7 @@ import { Course } from "./../model/course";
 import { Injectable } from "@angular/core";
 import { createHttpObservable } from "./util";
 import { map, tap } from "rxjs/operators";
+import { fromPromise } from "rxjs/internal-compatibility";
 
 @Injectable({
   providedIn: "root",
@@ -31,6 +32,27 @@ export class Store {
   filterByCategory(category: string) {
     return this.Courses$.pipe(
       map((courses) => courses.filter((course) => course.category == category))
+    );
+  }
+
+  saveCourse(CourseId: number, changes): Observable<any> {
+    const courses = this.subject.getValue();
+    const courseIndex = courses.findIndex((course) => course.id == CourseId);
+    const newCourses = courses.splice(0);
+    newCourses[courseIndex] = {
+      ...courses[courseIndex],
+      ...changes,
+    };
+    this.subject.next(newCourses);
+
+    return fromPromise(
+      fetch(`api/courses/${CourseId}`, {
+        method: "PUT",
+        body: JSON.stringify(changes),
+        headers: {
+          "content-type": "application/json",
+        },
+      })
     );
   }
 }
